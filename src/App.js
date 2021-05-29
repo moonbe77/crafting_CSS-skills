95a8b7460c024e31fccbeb58f948fbce5e6d5ab3
Bernardo <moonbe77@gmail.com>
2021-05-29 13:33:21 +1000

    add isPalindrome func

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
 import style from './App.module.css';
 
 function App() {
+
+ function isPalindrome(head)
+  {
+      let input = head
+      let res = false
+
+      for (let i = 0; i < input.length; i++){
+            if(input.length > 1){
+            let first = input.shift()
+            let last = input.pop()
+            first === last ? res = true : res = false
+            }            
+          }          
+          return res
+      }
+
+     console.log(isPalindrome([1,2,1,2,1]))
+
   return (
     <div className={style.App}>
      <div>
